404965f8ebeca961d54ed6bb2897bda87eeaf99d
Matteo Giuliano <matteo@giuliano.family>
2026-04-01 08:12:01 +0200

    Ho scritto i primi file per la connessione al database che andrò a fare inizialmente nel backend

---
+++ backend/db-connection.js
@@ -0,0 +1,28 @@
+import mysql from 'mysql2/promise'
+import fs from 'fs'
+import { parseINIString } from './../backend/public/js/utilities.js'
+import { userInfo } from 'os'
+
+const __dirname = import.meta.dirname
+
+let pool
+
+/**
+ * Questa funzione permette la connessione al database di ByteNote
+ */
+export function connect() {
+    if (pool) return pool
+
+    const data = fs.readFileSync(__dirname + '/data.dat', 'utf-8')
+    const js_ini = parseINIString(data)
+
+    pool = mysql.createPool({
+        host:               js_ini.Database.host,
+        user:               js_ini.Database.user,
+        password:           js_ini.Database.password,
+        waitForConnections: true,
+        connectionLimit:    10
+    })
+
+    return pool
+}
---
+++ backend/db-utilities.js
@@ -0,0 +1,17 @@
+import { connect } from './db-connection'
+
+/**
+ * Questa funzione esegue le query che li passiamo
+ * @param {*} query 
+ * @param {*} params 
+ */
+export async function sql(query, params = []) {
+    const pool = connect()
+
+    try {
+        const [rows] = await pool.query(query, params)
+        return rows
+    } catch (err) {
+        throw err
+    }
+}
