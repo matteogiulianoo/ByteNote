c1efbe2df814f4c070c5ffcf241cf406f53d6526
Matteo Giuliano <matteo@giuliano.family>
2026-04-01 11:22:43 +0200

    Inizio a sviluppare la home

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,7 +10,21 @@ const router = express.Router()
 router.post('/login', async (req, res) => {
     const { email, password } = req.body
 
-    // Verififica con il database
+    try {
+        const rows = await sql('SELECT email, password_hash FROM bn_user WHERE email = ?', [email])
+        if (rows.length === 0) return res.status(401).json({ errore: 'Credenziali non valide' })
+
+        const user = rows[0]
+
+        // Verifico la password
+        const password_check = await bcrypt.compare(password, user.password_hash)
+        if (!password_check) return res.status(401).json({ errore: 'Credenziali non valide' })
+
+        res.json({ successo: true })
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({ errore: 'Errore proveniente dal server' })
+    }
 })
 
 /**
@@ -22,9 +36,7 @@ router.post('/register', async (req, res) => {
     try {
         // Verifico se l'email all'interno del database esiste davvero
         const email_exist = await sql('SELECT id FROM bn_user WHERE email = ?', [email])
-        if (email_exist.length > 0) {
-            return res.status(409).json({ errore: 'Email già registrata dentro il nostro sistema' })
-        }
+        if (email_exist.length > 0) return res.status(409).json({ errore: 'Email già registrata dentro il nostro sistema' })
 
         const hash_password = await bcrypt.hash(password, 10)
         await sql('INSERT INTO bn_user (name, email, password_hash) VALUES (?, ?, ?)',
--- frontend/routes/home.js
+++ frontend/routes/home.js
@@ -6,10 +6,7 @@ const API_URL = 'http://localhost:4000'
 
 router.get('/', isAuthenticated, async (req, res) => {
     try {
-        const response = await fetch(`${API_URL}/api/utenti`)
-        const { dati } = await response.json()
-
-        res.render('pages/home', { utenti: dati })
+        res.render('pages/home', {})
     } catch (e) {
         console.error(e.message)
         res.status(500).send('Errore proveniente dal server backend')
